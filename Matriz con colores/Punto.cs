03c9f9c41031f3a8412c05b59f9b2475996f93b6
Diego <diego.chavez.barja@gmail.com>
2016-10-03 21:57:08 -0300

    Revert "Revert "Programa base""
    This reverts commit 21fa18381968c4bd9b7ced56fe27b71573c2083b.

---
+++ Matriz con colores/Punto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz_con_colores
+{
+    public class Punto
+    {
+        public int x;
+        public double y;
+    }
+}
